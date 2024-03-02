a310d8042d3c070d42baefcb32cfdd1e699e8b5d
Guhan Sivaji <55121697+gogitguhan@users.noreply.github.com>
2024-03-02 10:39:17 -0500

    Update get_access_token_and_print_cypher_stmts.sh

--- get_access_token_and_print_cypher_stmts.sh
+++ get_access_token_and_print_cypher_stmts.sh
@@ -2,6 +2,8 @@ echo 'Getting Access Token from Azure Active Directory...'
 
 export ACCESS_TOKEN=$(az account get-access-token --resource https://storage.azure.com/ --query "accessToken" --output tsv)
 
+export FABRIC_LH_PATH='https://onelake.dfs.fabric.microsoft.com/guhanworkspace/myLakehouse.Lakehouse'
+
 git reset --hard
 
 git pull https://github.com/neo4j-partners/neo4j-microsoft-fabrlc.git
