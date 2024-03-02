6e861b34b01b460234487f3c799d2117c377e5a2
Guhan Sivaji <55121697+gogitguhan@users.noreply.github.com>
2024-03-02 10:30:45 -0500

    Update get_access_token_and_print_cypher_stmts.sh

--- get_access_token_and_print_cypher_stmts.sh
+++ get_access_token_and_print_cypher_stmts.sh
@@ -6,7 +6,7 @@ git reset --hard
 
 git pull https://github.com/neo4j-partners/neo4j-microsoft-fabrlc.git
 
-envsubst '$ACCESS_TOKEN,$NEO4J_DB_USER_NAME,$NEO4J_DB_PASSWORD,$NEO4J_DB_SERVER_URL' < load_from_lakehouse.cypher > new_load_from_lakehouse.cypher
+envsubst '$ACCESS_TOKEN,$FABRIC_LH_PATH' < load_from_lakehouse.cypher > new_load_from_lakehouse.cypher
 
 clear
 
